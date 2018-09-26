9ca24a12a064a01c081409ea581d9ad0f3fca19f
iwa <ivanzarudny@gmail.com>
2018-09-26 21:46:33 +0500

    Now accepting negative numbers in earthquakes_2011 data

--- classwork3/regex.R
+++ classwork3/regex.R
@@ -2,8 +2,8 @@
 anss <- readLines("https://raw.githubusercontent.com/SergeyMirvoda/MD-DA-2017/master/data/earthquakes_2011.html", warn=FALSE)
 
 # Выберите строки, которые содержат данные с помощью регулярных выражений и функции grep
-pattern <- "^\\d+/\\d+/\\d+\\s\\d+:\\d+:\\d+\\.\\d+,\\d+\\.\\d+,\\d+\\.\\d+,\\d+\\.\\d+,\\d+\\.\\d+,\\w+,\\d+,,,\\d+\\.\\d+,\\w+,\\d+$"
-#           |  full year  |  |         time       |           |           |           |           |    |    |||           |    |    |
+pattern <- "^\\d+/\\d+/\\d+\\s\\d+:\\d+:\\d+\\.\\d+,-?\\d+\\.\\d+,-?\\d+\\.\\d+,-?\\d+\\.\\d+,-?\\d+\\.\\d+,\\w+,\\d+,,,-?\\d+\\.\\d+,\\w+,\\d+$"
+#           |   full year   |  |        time       |             |             |             |             |    |    |||             |    |    |
 
 result <- grep(pattern, anss, value = TRUE)
 
