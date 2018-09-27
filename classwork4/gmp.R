3321004da36148e3464aa353d076dad4327c01b1
Rozz <ivanzarudny@gmail.com>
2018-09-27 10:17:48 +0500

    Classwork 4 WIP

--- classwork4/gmp.R
+++ classwork4/gmp.R
@@ -1,7 +1,10 @@
-#Загрузите данные в датафрейм. Адрес: github    https://raw???путь_к_файлу_найдите_сами???/data/gmp.dat 
-gmp <- ...
+# Загрузите данные в датафрейм /data/gmp.dat 
+data <- read.table("https://raw.githubusercontent.com/SergeyMirvoda/MD-DA-2018/master/data/gmp.dat", header = TRUE)
+gmp <- data.frame(data)
+gmp$pop <- gmp$gmp / gmp$pcgmp
+y_st <- 6611
 
-estimate.scaling.exponent <- function(a, y0=6611, response=gmp$pcgmp,
+estimate.scaling.exponent <- function(a, y0=y_st, response=gmp$pcgmp,
                                         predictor = gmp$pop, maximum.iterations=100, deriv.step = 1/100,
                                         step.scale = 1e-12, stopping.deriv = 1/100) {
   mse <- function(a) { mean((response - y0*predictor^a)^2) }
@@ -14,12 +17,28 @@ estimate.scaling.exponent <- function(a, y0=6611, response=gmp$pcgmp,
               converged=(iteration < maximum.iterations))
   return(fit)
 }
-#Пример вызова с начальным занчением a
-estimate.scaling.exponent(0.15)
 
-#С помошью полученного коэффициента постройте кривую (функция curve) зависимости
+# Пример вызова с начальным занчением a
+k <- estimate.scaling.exponent(0.15)
 
-#Удалите точку из набора исходных данных случайным образом, как изменилось статистическая оценка коэффициента a?
+# С помошью полученного коэффициента постройте кривую (функция curve) зависимости
+curve(y_st * x ^ estimate.scaling.exponent(0.15, y0 = y_st)$a,
+      xlab = "Население",
+      ylab = "ВВП / Человек",
+      from = 1,
+      to = 1000)
 
-#Запустите оценку несколько раз с разных стартовых точек. Как изменилось значение a?
+# Удалите точку из набора исходных данных случайным образом, как изменилось статистическая оценка коэффициента a?
+set.seed(86548435, kind = "Knuth-TAOCP-2002")
+gmp <- gmp[-c(sample.int(nrow(gmp), 1))]
+k_changed <- estimate.scaling.exponent(0.15)
 
+k$a - k_changed$a
+
+# Запустите оценку несколько раз с разных стартовых точек. Как изменилось значение a?
+k_1000 <- estimate.scaling.exponent(0.15, 1000)
+k_2000 <- estimate.scaling.exponent(0.15, 2000)
+k_3000 <- estimate.scaling.exponent(0.15, 3000)
+k_10000 <- estimate.scaling.exponent(0.15, 10000)
+
+# TODO: add graph
