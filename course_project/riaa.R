4f2e3fcc08235bd360e3ac7225a76ebad7dc7a94
Zdrast <e_gor-97@mail.ru>
2019-01-06 18:00:41 +0500

    1
    1

--- course_project/riaa.R
+++ course_project/riaa.R
@@ -1,13 +1,30 @@
-table <- read.csv2('data/riaa.csv', header=TRUE, sep=';')
+library("spatstat")
+library("forecast")
+library("lubridate")
+#table <- read.csv2('data/riaa.csv', header=TRUE, sep=';')
 
 # Converting columns
-table$Certification.date <- as.Date(table$Certification.date, '%B %d, %Y')
-table$Release.date <- as.Date(table$Release.date, '%B %d, %Y')
+#table$Certification.date <- as.Date(table$Certification.date, '%B %d, %Y')
+#table$Release.date <- as.Date(table$Release.date, '%B %d, %Y')
 
-value <- regexpr("(\\d+|\\d+\\.\\d+)", table$Certified.Units)
-value <- regmatches(table$Certified.Units, value)
-table$Certified.Units <- as.double(value) * 1e6
+#value <- regexpr("(\\d+|\\d+\\.\\d+)", table$Certified.Units)
+#value <- regmatches(table$Certified.Units, value)
+#table$Certified.Units <- as.double(value) * 1e6
 
-summary(table)
+#summary(table)
 
-plot(table$Release.date ~ table$Certification.date)
+#plot(table$Release.date ~ table$Certification.date)
+
+load('riaa.Rda')
+
+genre<-table[table$Genre=="ROCK",]
+genre$Award<-c(1,2,3)[genre$Award]
+y=ts(genre$Certified.Units, start=c(2012, yday("2012-01-01")),end=c(2018, yday("2018-01-01")), frequency=365)
+y.stlf<-stlf(y,h=365)
+plot(y.stlf)
+Q<-ppp(x=genre$Release.date, y=genre$Award)
+Q <- ppp(genre)
+ppm(Q ~ 1)
+fit<-arima(y, order=c(1,0,12))
+plot(forecast(fit, 365))
+accuracy(fit)
