30576d460ec6180ac83748fc694d33f062450ae2
iwa <ivanzarudny@gmail.com>
2019-01-09 18:11:49 +0500

    First draft version of R prediction model

--- course_project/model.R
+++ course_project/model.R
@@ -1,14 +1,70 @@
 library(optparse)
+library(dplyr)
 option_list <- list(
     make_option(c("--genre"), action="store", default=NA, type='character', help="Genre")
 )
 opt = parse_args(OptionParser(option_list=option_list))
 
 load('../riaa.Rda')
+table %>%
+  filter(Genre != 'UNASSIGNED' & Genre != ' None') -> table
+
+
+generate.model <- function (timeseries) {
+  model <- Arima(timeseries, seasonal=c(1,1,0), include.drift=T)
+  return(model)
+}
+
+test.model <- function (timeseries) {
+  model.train <- window(timeseries, start=2000, end=2015)
+  model.test <- window(timeseries, start=2015+1/12, end=2018+11/12)
+  
+  model <- generate.model(model.train)
+  
+  acc <- accuracy(forecast(model, h=length(model.test)), model.test)
+  print('!&ME')
+  print(acc[,1])
+  print('!&RMSE')
+  print(acc[,2])
+  print('!&MAE')
+  print(acc[,3])
+  print('!&MPE')
+  print(acc[,4])
+  print('!&MAPE')
+  print(acc[,5])
+  print('!&MASE')
+  print(acc[,6])
+  print('!&ACF1')
+  print(acc[,7])
+}
+
+
 
 if (opt$genre == 'all') {
-    print(levels(table$Genre))
+  print(levels(table$Genre))
 } else {
-    table <- table[table$Genre==opt$genre,]
-    print(table[1,])
+  library(spatstat)
+  library(forecast)
+  library(lubridate)
+  
+  table %>%
+    filter(Genre == opt$genre) %>%
+    filter(Release.date > as.Date("2000-01-01")) %>%
+    filter(Release.date < as.Date("2018-12-31")) %>%
+    filter(!is.na(Certified.Units)) -> table
+  
+  timeseries <- ts(table$Certified.Units, start=2000, end=2018+11/12, frequency=12)
+  
+  model <- generate.model(timeseries)
+  model.forecast <- forecast(model, h=24)
+  prediction.sales <- model.forecast$upper[,2][12:23]
+  
+  pks <- which(diff(sign(diff(prediction.sales, na.pad = FALSE)), na.pad = FALSE) < 0) + 2
+  peaks <- pks[prediction.sales[pks - 1] - prediction.sales[pks] > 0] - 1
+  dates <- round(peaks / 12 * 365)
+  
+  print('!&dates')
+  print(dates)
+  
+  test.model(timeseries)
 }
--- course_project/model_packages.R
+++ course_project/model_packages.R
@@ -1,5 +1,5 @@
 # Append libraries that will be used for model generation on server side
 
-packages <- c('optparse')
+packages <- c('curl', 'dplyr', 'optparse', 'spatstat', 'forecast', 'lubridate')
 
 install.packages(packages, repos='https://cran.rstudio.com')
