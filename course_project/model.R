41c54f68bc9cfbfb18bb947ff0b1d7bfc610fffe
iwa <ivanzarudny@gmail.com>
2019-01-09 20:43:06 +0500

    Filtering table from unused columns

--- course_project/model.R
+++ course_project/model.R
@@ -10,7 +10,8 @@ setwd('..') # for script
 load('riaa.Rda')
 table %>%
   filter(Genre != 'UNASSIGNED') %>%
-  filter(Genre != ' None') -> table
+  filter(Genre != ' None') %>%
+  select(-c(Award, Certification.date, ID, Artist, Title, Label, Format, Category, Type))-> table
 
 table$Genre = factor(table$Genre)
 
@@ -64,7 +65,8 @@ if (opt$genre == 'all') {
     filter(Genre == opt$genre) %>%
     filter(Release.date > as.Date('2000-01-01')) %>%
     filter(Release.date < as.Date('2018-12-31')) %>%
-    filter(!is.na(Certified.Units)) -> table
+    filter(!is.na(Certified.Units)) %>%
+    select(-c(Genre)) -> table
   
   timeseries <- ts(table$Certified.Units, start=2000, end=2018+11/12, frequency=12)
   
